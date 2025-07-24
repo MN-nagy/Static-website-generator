a503816b825a5b9bc805c09892d4b4fcd57ef291
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-07-24 17:31:34 +0300

    DSA Tests passed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def get_follower_prediction(follower_count: int, influencer_type: str, num_months: int) -> int:
-    r = 2
-    if influencer_type == "fitness":
-        r = 4
-    elif influencer_type == "cosmetic":
-        r = 3
-
-    return  follower_count * r**num_months
+def does_name_exist(first_names, last_names, full_name):
+    for f_name in first_names:
+        for l_name in last_names:
+            if full_name.startswith(f_name):
+                if full_name.endswith(l_name):
+                    return True
+                continue
+            continue
+    return False
--- test_main.py
+++ test_main.py
@@ -1,35 +1,60 @@
 from main import *
+from commit import commit
 
 run_cases = [
-    (10, "fitness", 1, 40),
-    (10, "fitness", 2, 160),
-    (12, "cosmetic", 4, 972),
+    (100, 100, "bob0 gonzalez0", True),
+    (500, 500, "maria1 smith1", True),
 ]
 
 submit_cases = run_cases + [
-    (15, "business", 4, 240),
-    (10, "fitness", 5, 10240),
-    (10, "fitness", 6, 40960),
-    (10, "fitness", 7, 163840),
-    (10, "fitness", 8, 655360),
-    (10, "tech", 9, 5120),
+    (1000, 1000, "bob500 smith1", False),
+    (2000, 2000, "bob1999 wagner1998", False),
+    (3000, 3000, "sally2999 smith2998", True),
 ]
 
 
-def test(input1, input2, input3, expected_output):
+def test(num_fnames, num_lnames, check_name, expected_output):
     print("---------------------------------")
     print(f"Inputs:")
-    print(f" * Follower count: {input1}")
-    print(f" * Influencer type: {input2}")
-    print(f" * Number of months: {input3}")
+    print(f" * num first_names: {num_fnames}")
+    print(f" * num last_names: {num_lnames}")
+    print(f" * looking for name: {check_name}")
     print(f"Expecting: {expected_output}")
-    result = get_follower_prediction(input1, input2, input3)
+    fnames = get_first_names(num_fnames)
+    lnames = get_last_names(num_lnames)
+    result = does_name_exist(fnames, lnames, check_name)
     print(f"Actual: {result}")
     if result == expected_output:
         print("Pass")
         return True
     print("Fail")
     return False
+
+
+def get_first_names(num):
+    names = []
+    for i in range(num):
+        m = i % 3
+        if m == 0:
+            names.append(f"bob{i}")
+        elif m == 1:
+            names.append(f"maria{i}")
+        elif m == 2:
+            names.append(f"sally{i}")
+    return names
+
+
+def get_last_names(num):
+    names = []
+    for i in range(num):
+        m = i % 3
+        if m == 0:
+            names.append(f"gonzalez{i}")
+        elif m == 1:
+            names.append(f"smith{i}")
+        elif m == 2:
+            names.append(f"wagner{i}")
+    return names
 
 
 def main():
@@ -44,6 +69,7 @@ def main():
             failed += 1
     if failed == 0:
         print("============= PASS ==============")
+        commit()
     else:
         print("============= FAIL ==============")
     if skipped > 0:
