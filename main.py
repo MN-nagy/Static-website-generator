427509f8138a9600d8c6bde44a45941a0938088b
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-07-25 03:30:25 +0300

    DSA Tests passed

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def insertion_sort(nums):
-    for i in range(1, len(nums)):
-        j = i
-        while j > 0 and nums[j - 1] > nums[j]:
-            nums[j - 1], nums[j] = nums[j], nums[j - 1]
-            j -= 1
-    return nums
+def quick_sort(nums, low, high):
+    if low < high:
+        middle = partition(nums, low, high)
+        quick_sort(nums, low, middle - 1)
+        quick_sort(nums, middle + 1, high)
+
+
+def partition(nums, low, high):
+    pivot = high
+    i = low - 1
+    for j in range(low, high):
+        if nums[j] < nums[pivot]:
+            i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+    nums[i + 1], nums[pivot] = nums[pivot], nums[i + 1]
+    return i + 1
