c1746ff80141dd839adf512b5ba82fe31ea4d8e9
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-08-03 20:25:06 +0300

    DSA Tests passed

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import math
 def prime_factors(n):
     factors = []
     factored = n
-    while True:
-        if factored % 2 != 0:
-            break
+    while factored % 2 == 0:
         factored //= 2
         factors.append(2)
     for i in range(3, int(math.sqrt(factored)) + 1, 2):
